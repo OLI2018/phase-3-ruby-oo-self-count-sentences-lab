6f439884cfa906ef282683f206b864a82ed3d94d
Oleg <roxvemm2014@gmail.com>
2021-09-18 19:25:37 -0700

    initial commit

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    self.end_with?(".") ? true : false 
+    end
 
   def question?
-
+    self.end_with?("?") ? true : false 
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false 
   end
 
   def count_sentences
 
+     sentences = 0
+     punctuation = [".", "?", "!"]
+     self.chars.each.with_index do |char, index|
+       if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
 end
+
+#binding.pry
